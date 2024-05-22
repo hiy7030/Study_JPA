b0959556a9be282999c0d0782a38a5f72289320d
hiy7030 <hiy7030@gmail.com>
2024-05-22 21:38:13 +0900

    JPQL 맛보기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,23 +2,25 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
-        // 서버 실행 시, 생성되어 서버가 종료될 때 종료
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        // 클라이언트의 요청이 올때마다 생성되고 종료
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("bye");
+            // 전체 회원 조회 : JPQL 사용
+            List<Member> members = em.createQuery("select m from Member as m", Member.class).getResultList();
 
-            // 수정을 했으니 저장을 해야 하나? -> JPA는 하나의 트랜잭션에서 이루어진 변경을 감지하여 자동으로 변경해준다.
-//            em.persist(findMember);
+            for(Member m : members) {
+                System.out.println("id: "+ m.getId() + ", name: " + m.getName());
+            }
+
 
             tx.commit();
         } catch (Exception e) {
