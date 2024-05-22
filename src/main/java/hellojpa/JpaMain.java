f237df26698f8879d104373b9f32912fe5b8f877
hiy7030 <hiy7030@gmail.com>
2024-05-22 21:21:54 +0900

    jpa 정석 코드

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,12 +5,26 @@ import jakarta.persistence.*;
 public class JpaMain {
 
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        // jpa 트랜잭션 시작
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setName("hello2");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
